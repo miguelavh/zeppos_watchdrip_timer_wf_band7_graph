4a29fc3ca2900b7b2b872d1b6141ef3259c0b828
4RK4N <4rk4nus@googlemail.com>
2023-01-28 22:33:08 +0100

    test data for external statusline

--- utils/watchdrip/model/externalData.js
+++ utils/watchdrip/model/externalData.js
@@ -1,3 +1,6 @@
+import {TEST_DATA} from "../../config/constants";
+import {MINUTE_IN_MS} from "../../../shared/date";
+
 export class ExternalData {
     constructor(statusLine, time) {
         this.statusLine = statusLine;
@@ -19,6 +22,9 @@ export class ExternalData {
     }
 
     static createEmpty() {
+        if (TEST_DATA) {
+            return new ExternalData("115% 5.43U 28g", Date.now() - (6 * MINUTE_IN_MS));
+        }
         return new ExternalData("", null);
     }
 }
